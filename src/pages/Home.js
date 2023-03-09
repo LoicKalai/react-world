5d3bc41c48d457b9aaa384f535369ad783db152c
LoicKalai <loic.kalaidjoglou@icloud.com>
2023-03-09 19:06:07 +0100

    Mise en place composants et pages

---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>ACCEUIL</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        aliquid itaque eaque praesentium veniam sit hic soluta voluptates
+        excepturi dolor sed nobis debitis quaerat, atque quidem? Totam suscipit
+        eveniet dignissimos! Quibusdam, sed minus quae molestias aspernatur
+        nemo, praesentium quia aperiam harum architecto fugit ipsa provident
+        voluptatibus labore, dolores est. Veritatis distinctio earum error
+        nobis, beatae ipsum repudiandae sapiente expedita architecto! Facere et
+        aliquam placeat asperiores nisi quaerat dolore neque debitis. Magni ab
+        asperiores iusto voluptatum dolorem expedita odit vel? Blanditiis
+        voluptate quo impedit soluta atque optio, officiis similique. Nemo,
+        recusandae.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est sit
+        numquam nihil, corporis voluptatem aliquam rem provident neque, unde
+        consectetur cum quia suscipit necessitatibus modi in ex quasi quisquam
+        veniam. Soluta saepe amet mollitia. Totam voluptate laudantium
+        perferendis nesciunt sapiente. Rem facere atque repellendus praesentium,
+        totam odio pariatur delectus distinctio dolorum quod quas deserunt enim
+        quis illo repellat incidunt mollitia. Repellendus ratione libero a
+        fugiat nulla iste quo necessitatibus velit culpa ipsa facere non
+        incidunt laudantium, sapiente praesentium tenetur error aperiam totam
+        quas neque debitis autem rem! Ratione, consequuntur iste! Corporis non
+        ipsum tempora facere nesciunt quisquam iste laborum! Quibusdam beatae
+        ullam voluptatum. Soluta ab, sunt at quas eaque fugit qui repellendus
+        laborum autem quis, fugiat minima voluptate eum laboriosam!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
